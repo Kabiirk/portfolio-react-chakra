c1a97b07d08310ab6791cd5591f305219c69030e
Kabiir <kabiirk@gmail.com>
2021-06-27 23:13:22 +0530

    Repurposed skills into projects section

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,41 +8,72 @@ function Profile() {
     const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
 
     return (
-        <Flex direction={isNotSmallerScreen ? "row":"column"} w="100%" maxWidth={{base:"100vh", md:"130vh", lg:"130vh", xl:"130vh"}}>
-            <Box alignSelf="center" pr="10" py="16">
-                <Heading fontWeight="extrabold" color="cyan.500" size="4xl">
-                    7+
-                </Heading>
-                <Text fontSize="2xl" color="gray.400">Years of Experience</Text>
-            </Box>
-            <Box alignSelf="center" px="0" py="16">
-                <Flex direction={isNotSmallerScreen ? "row" : "column"} mt={8} >
-                    <Flex rounded="xl" direction="column" mt={4} bg="blue.400" h="30vh" w="30vh" justify="flex-end">
-                        <Icon color="white" p="4" as={DiAndroid} w="24" h="24" />
-                        <Text color="white" p="4" fontSize="xl" fontWeight="semibold">
-                            Android Apps
-                        </Text>
+        <Box>
+            <Heading fontSize="3xl" fontWeight="bold" color="gray.400" textAlign="center" py="0" pt="10">My Projects</Heading>
+            <Text fontSize="1xl" color="gray.600" textAlign="center">Some of the projects I have done.</Text>
+            <Flex direction={isNotSmallerScreen ? "row":"column"} w="100%" maxWidth={{base:"100vh", md:"130vh", lg:"130vh", xl:"130vh"}}>
+                {/* <Box alignSelf="center" pr="10" py="16">
+                    <Heading fontWeight="extrabold" color="cyan.500" size="4xl">
+                        7+
+                    </Heading>
+                    <Text fontSize="2xl" color="gray.400">Years of Experience</Text>
+                </Box> */}
+                <Box alignSelf="center" px="5" py="0">
+                    <Flex direction={isNotSmallerScreen ? "row" : "column"} >
+                        <Flex rounded="xl" direction="column" mt={4} bg="blue.400" h="40vh" w="40vh" justify="flex-end">
+                            <Icon color="white" p="4" as={DiAndroid} w="24" h="24" />
+                            <Text color="white" p="4" fontSize="xl" fontWeight="semibold">
+                                Android Apps
+                            </Text>
+                        </Flex>
+                        <Flex rounded="xl" direction="column" mt={4} ml={isNotSmallerScreen ? 4 : 0}
+                            bg="gray.100" h="40vh" w="40vh" justify="flex-end" _hover={{ bg: "teal.400", }}>
+                            <Icon color="black" p="4" as={DiCodeigniter} w="24" h="24" />
+                            <Text color="black" p="4" fontSize="xl" fontWeight="semibold">
+                                Flutter Apps
+                            </Text>
+                        </Flex>
+                        <Flex rounded="xl" direction="column" mt={4} ml={isNotSmallerScreen ? 4 : 0}
+                            bg="gray.100" h="40vh" w="40vh" justify="flex-end"
+                            _hover={{ bg: "green.400", }}
+                        >
+                            <Icon as={DiWebplatform} p="4" w="24" h="24" color="black" />
+                            <Text color="black" p="4" fontSize="xl" fontWeight="semibold">
+                                Web Apps
+                            </Text>
+                        </Flex>
                     </Flex>
-                    <Flex rounded="xl" direction="column" mt={4} ml={isNotSmallerScreen ? 4 : 0}
-                        bg="gray.100" h="30vh" w="30vh" justify="flex-end" _hover={{ bg: "teal.400", }}>
-                        <Icon color="black" p="4" as={DiCodeigniter} w="24" h="24" />
-                        <Text color="black" p="4" fontSize="xl" fontWeight="semibold">
-                            Flutter Apps
-                        </Text>
+                </Box>
+            </Flex>
+            <Flex direction={isNotSmallerScreen ? "row":"column"} w="100%" maxWidth={{base:"100vh", md:"130vh", lg:"130vh", xl:"130vh"}}>
+                <Box alignSelf="center" px="5">
+                    <Flex direction={isNotSmallerScreen ? "row" : "column"} >
+                        <Flex rounded="xl" direction="column" mt={4} bg="blue.400" h="40vh" w="40vh" justify="flex-end">
+                            <Icon color="white" p="4" as={DiAndroid} w="24" h="24" />
+                            <Text color="white" p="4" fontSize="xl" fontWeight="semibold">
+                                Android Apps
+                            </Text>
+                        </Flex>
+                        <Flex rounded="xl" direction="column" mt={4} ml={isNotSmallerScreen ? 4 : 0}
+                            bg="gray.100" h="40vh" w="40vh" justify="flex-end" _hover={{ bg: "teal.400", }}>
+                            <Icon color="black" p="4" as={DiCodeigniter} w="24" h="24" />
+                            <Text color="black" p="4" fontSize="xl" fontWeight="semibold">
+                                Flutter Apps
+                            </Text>
+                        </Flex>
+                        <Flex rounded="xl" direction="column" mt={4} ml={isNotSmallerScreen ? 4 : 0}
+                            bg="gray.100" h="40vh" w="40vh" justify="flex-end"
+                            _hover={{ bg: "green.400", }}
+                        >
+                            <Icon as={DiWebplatform} p="4" w="24" h="24" color="black" />
+                            <Text color="black" p="4" fontSize="xl" fontWeight="semibold">
+                                Web Apps
+                            </Text>
+                        </Flex>
                     </Flex>
-                    <Flex rounded="xl" direction="column" mt={4} ml={isNotSmallerScreen ? 4 : 0}
-                        bg="gray.100" h="30vh" w="30vh" justify="flex-end"
-                        _hover={{ bg: "green.400", }}
-
-                    >
-                        <Icon as={DiWebplatform} p="4" w="24" h="24" color="black" />
-                        <Text color="black" p="4" fontSize="xl" fontWeight="semibold">
-                            Web Apps
-                        </Text>
-                    </Flex>
-                </Flex>
-            </Box>
-        </Flex>
+                </Box>
+            </Flex>
+        </Box>
     )
 }
 
